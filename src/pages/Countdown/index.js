a12bef1365b012a54c18368d99ab34c8719aeacb
William Berbet <wiiliamb@hotmail.com>
2025-02-07 12:14:37 -0300

    inscrições encerradas + ativando mensagem

--- src/pages/Countdown/index.js
+++ src/pages/Countdown/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Counter from "../../components/Counter";
 import useCountdown from "../../hooks/useCountdown";
-import { Link } from "react-router-dom";
-import Button from "../../components/Button";
+/* import { Link } from "react-router-dom";
+import Button from "../../components/Button"; */
 import Title from "../../components/Title";
 import logo from "../../assets/logo.png"
 
@@ -29,28 +29,28 @@ export default function Countdown() {
                     <Counter title='Segundos' number={second} />
                 </div>
 
-                <div className='button1'>
+                {/* <div className='button1'>
                     <Link to="/register">
                         <Button children="GARANTA JÁ SUA VAGA!" />
 
                     </Link>
-                </div>
+                </div> */}
 
                 {/* <div className='action'>
           <a href='https://forms.gle/qbe9VkYMyZH6QiPs6'><button type='text'>garanta já a sua vaga</button></a>
           <p>clique no botão para fazer a inscrição!</p>
         </div> */}
 
-                {/* <div className='over'>
-          <span className='umapena'>
-            AS INSCRIÇÕES JÁ SE ENCERRARAM, UMA PENA!
-          </span>
+                <div className='over'>
+                    <span className='umapena'>
+                        AS INSCRIÇÕES JÁ SE ENCERRARAM, UMA PENA!
+                    </span>
 
-          <span className='whats'>Mas se precisar falar com a gente, <a href='https://api.whatsapp.com/send?phone=5519989318887&text=Oi!%20Queria%20conversar%20sobre%20o%20Acampa.'>
-            CLIQUE AQUI
-          </a>
-          </span>
-        </div> */}
+                    <span className='whats'>Mas se precisar falar com a gente, <a href='https://api.whatsapp.com/send?phone=5519989318887&text=Oi!%20Queria%20conversar%20sobre%20o%20Acampa.'>
+                        CLIQUE AQUI
+                    </a>
+                    </span>
+                </div>
 
             </div>
         </div>
